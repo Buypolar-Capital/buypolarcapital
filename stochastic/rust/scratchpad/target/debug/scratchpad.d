5525cf7fb3e546b6497686f605d3ad74e45d383e
egil10 <egilfure@gmail.com>
2025-04-19 01:42:53 +0200

    a bunch of movement

--- stochastic/rust/scratchpad/target/debug/scratchpad.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ofurn\Dokumenter\Github\buypolarcapital\stochastic\rust\scratchpad\target\debug\scratchpad.exe: C:\Users\ofurn\Dokumenter\Github\buypolarcapital\stochastic\rust\scratchpad\src\main.rs
