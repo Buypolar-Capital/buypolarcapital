382fca7f8bd1bf95c980272842da2600b11ffe71
egil10 <egilfure@gmail.com>
2025-06-27 02:16:04 +0200

    new

--- NBIM/utils/load_data.py
+++ NBIM/utils/load_data.py
@@ -1,22 +1,23 @@
 import os
 import pandas as pd
-
-@st.cache_data
-def load_fixed_income_data():
-    data_path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_bonds.csv")
-    return pd.read_csv(data_path, index_col="Date", parse_dates=True)
+import streamlit as st
 
 @st.cache_data
 def load_equity_data():
-    data_path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_equities.csv")
-    return pd.read_csv(data_path, index_col=0, parse_dates=True)
+    path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_equities.csv")
+    return pd.read_csv(path, index_col=0, parse_dates=True)
+
+@st.cache_data
+def load_fixed_income_data():
+    path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_bonds.csv")
+    return pd.read_csv(path, index_col="Date", parse_dates=True)
 
 @st.cache_data
 def load_real_estate_data():
-    data_path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_realestate.xlsx")
-    return pd.read_excel(data_path, index_col=0, parse_dates=True, decimal=",")
+    path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_realestate.xlsx")
+    return pd.read_excel(path, index_col=0, parse_dates=True, decimal=",")
 
 @st.cache_data
 def load_renewable_data():
-    data_path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_renewable.xlsx")
-    return pd.read_excel(data_path, index_col=0, parse_dates=True, decimal=",")
+    path = os.path.join(os.path.dirname(__file__), "..", "data", "nbim_top10_renewable.xlsx")
+    return pd.read_excel(path, index_col=0, parse_dates=True, decimal=",")
